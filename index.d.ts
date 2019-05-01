e6b30987372e14cef87bf6ec12784ddab4e0b027
Niklas Higi <niklas@shroudedcode.com>
2019-05-01 12:30:38 +0200

    Fix type issues

--- index.d.ts
+++ index.d.ts
@@ -4,16 +4,8 @@ interface Attributes {
 	key?: string | number;
 }
 
-declare namespace DomChef {
-	export function createElement<P extends {}>(
-		type: DocumentFragment | string,
-		props?: Attributes & P | null,
-		...children: Node[]
-	): Element | DocumentFragment;
-
-	export type Fragment = DocumentFragment | Function;
-	export type h = typeof createElement;
-}
-
-declare const domChef: DomChef;
-export default domChef;
+export function h<P extends {}>(
+	type: DocumentFragment | string,
+	props?: Attributes & P | null,
+	...children: Node[]
+): Element | DocumentFragment;
